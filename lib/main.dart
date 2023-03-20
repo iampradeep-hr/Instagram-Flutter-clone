a97955c04cfb61d64938b66a05af8ae2d31ab0a5
Pradeep <iampradeephr@gmail.com>
2023-03-20 20:17:04 +0530

    Responsive Layout Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone_flutter/responsive/mobile_screen_layout.dart';
+import 'package:instagram_clone_flutter/responsive/responsive_layout_screen.dart';
+import 'package:instagram_clone_flutter/responsive/web_screen_layout.dart';
+import 'package:instagram_clone_flutter/utils/colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,10 +13,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
-    );
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "Instagram Clone",
+        theme: ThemeData.dark()
+            .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
+        home: const ResponsiveLayout(
+          webScreenLayout: WebScreenLayout(),
+          mobileScreenLayout: MobileScreenLayout(),
+        ));
   }
 }
-
-
